2650aabe5771d81db531c4c0170b490550c88b04
cuervolu <cuervolu@protonmail.com>
2023-01-02 15:19:47 -0300

    build: CalendarApp 1.5

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
-import { CalendarApp } from './CalendarApp'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { CalendarApp } from "./CalendarApp";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <HashRouter>
-    <CalendarApp />
-    </HashRouter>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <CalendarApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);
